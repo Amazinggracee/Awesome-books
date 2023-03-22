b3b8f7160c1ea72e100c05169450a56016c02e6e
zied2112 <zied-ci@hotmail.fr>
2023-03-22 15:28:59 +0100

    complete website

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const form = document.querySelector('.form');
-class Book {
-  constructor(title, author, index) {
-    this.title = title;
-    this.author = author;
-    this.index = index;
-  }
+const timeAndDate = document.querySelector('#time-and-date');
 
-  static displayBooks() {
-    const books = Book.getBooks();
-    books.forEach((book) => Book.addBookToList(book));
-  }
+const dateAndTime = () => {
+  setInterval(() => {
+    const date = new Date().toUTCString();
+    timeAndDate.innerHTML = date;
+  }, 0);
+};
+
+dateAndTime();
+
+class Book {
+  // constructor(title, author, index) {
+  //   this.title = title;
+  //   this.author = author;
+  //   this.index = index;
+  // }
+
+  // static displayBooks() {
+  //   const books = Book.getBooks();
+  //   books.forEach((book) => Book.addBookToList(book));
+  // }
 
   static addBookToList(book) {
     const library = document.querySelector('.library');
@@ -76,11 +87,52 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const author = document.querySelector('.input-author').value;
   const title = document.querySelector('.input-book').value;
+  const error = document.querySelector('.error-message');
   if (author === '' || title === '') {
-    alert('Please fill in all fields');
+    error.style.display = 'block';
   }
   const book = new Book(title, author);
   Book.addBookToList(book);
   Book.addBook(book);
   Book.clearFields();
+});
+
+const logo = document.querySelector('#logo');
+const list = document.querySelector('#list');
+const newbook = document.querySelector('#newbook');
+const contact = document.querySelector('#contact');
+const display = document.querySelector('#display');
+const contactPage = document.querySelector('#contactPage');
+logo.addEventListener('click', () => {
+  list.style.color = 'blue';
+  contact.style.color = 'black';
+  newbook.style.color = 'black';
+  display.style.display = 'block';
+  form.style.display = 'none';
+  contactPage.style.display = 'none';
+});
+list.addEventListener('click', () => {
+  list.style.color = 'blue';
+  contact.style.color = 'black';
+  newbook.style.color = 'black';
+  display.style.display = 'block';
+  form.style.display = 'none';
+  contactPage.style.display = 'none';
+});
+
+newbook.addEventListener('click', () => {
+  list.style.color = 'black';
+  contact.style.color = 'black';
+  newbook.style.color = 'blue';
+  form.style.display = 'flex';
+  display.style.display = 'none';
+  contactPage.style.display = 'none';
+});
+contact.addEventListener('click', () => {
+  list.style.color = 'black';
+  contact.style.color = 'blue';
+  newbook.style.color = 'black';
+  contactPage.style.display = 'block';
+  form.style.display = 'none';
+  display.style.display = 'none';
 });
